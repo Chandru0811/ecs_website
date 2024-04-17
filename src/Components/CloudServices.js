3517c4a2a400e71f4071b5ab698057a95c472776
Sumaiya-904 <sumaiyabee211@gmail.com>
2024-04-17 16:17:46 +0530

    product,cloud and solution pages on process

---
+++ src/Components/CloudServices.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import EcsCloudServices from "../assests/manageCloud.png";
+import EcsAws from "../assests/aws.png";
+import AwsDevops from "../assests/devops.png";
+import AwsArchitecture from "../assests/awsArchitect.png";
+import AwsMigration from "../assests/awsMigration.png";
+function CloudServices() {
+  return (
+    <section className='container-fluid mt-5' style={{ backgroundColor: "#E1FDFA" }}>
+    <div className='container'>
+        <div className='row pt-5 d-flex'>
+            <div className='col-md-6 col-12'>
+                <div className='row'>
+                    <div className='col-md-10 offset-md-1'>
+                        <p className="mt-5 text-start  fs-6" style={{ color: "#08A391" }}>Cloud  Services  Managemnet |  Cloud ECS Infoteh</p>
+                        <h3 className='d-flex text-start display-5 fw-medium' style={{ color: "rgba(0, 0, 0, 0.722)" }}>Leveraging  Benefits  of Cloud  Computing  with  Managed  Services  and Solutions</h3>
+                        <p className="pt-3 text-start fs-4">We're a Gartner recognised , triple certified managed cloud service provider. With our support,your teams our empowered to deliver more value with greater agility</p>
+                    </div>
+                </div>
+            </div>
+            <div className='col-md-6 col-12'>
+                <img className="img-fluid" src={EcsCloudServices} alt='manageCloud' />
+            </div>
+        </div>
+    </div>
+    <div className='container'>
+        <div className='row mt-5 d-flex'>
+            <div className='col-md-6 col-12'>
+                <img className="img-fluid" src={EcsAws} alt='ecsAws' />
+            </div>
+            <div className='col-md-6 col-12'>
+                <div className='row'>
+                    <div className='col-md-10 offset-md-1'>
+                        <p className="mt-5 text-start  fs-6" style={{ color: "#08A391" }}>Expert  Support  For  AWS Cloud  from  assessment  and  migration to  optimization  and  experiences </p>
+                        <h3 className='d-flex text-start display-5 fw-medium' style={{ color: "rgba(0, 0, 0, 0.722)" }}>AWS  Cloud  Formation</h3>
+                        <p className="pt-3 text-start fs-4">Scale your infrastructure worlwide and manage resources across all AWS accounts and regions through a single operations.Automate resource management across your organization with AWS service integration offering turnkey application distribution and govermance control.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className='container'>
+        <div className='row pt-5 d-flex'>
+            <div className='col-md-6 col-12'>
+                <div className='row'>
+                    <div className='col-md-10 offset-md-1'>
+                        <p className="mt-5 text-start  fs-6" style={{ color: "#08A391" }}>Efficient  Deployement | Optimal Security | Cost Optimization | High Scalability</p>
+                        <h3 className='d-flex text-start display-5 fw-medium' style={{ color: "rgba(0, 0, 0, 0.722)" }}>Become a Cloud DevOps Engineer</h3>
+                        <p className="pt-3 text-start fs-4">Companies are looking for talented DevOps engineers to remain competitive In this agile world.</p>
+                    </div>
+                </div>
+            </div>
+            <div className='col-md-6 col-12'>
+                <img className="img-fluid" src={AwsDevops} alt='awsDevops' />
+            </div>
+        </div>
+    </div>
+    <div className='container'>
+        <div className='row mt-5 d-flex'>
+            <div className='col-md-6 col-12'>
+                <img className="img-fluid" src={AwsArchitecture} alt='ecsAws' />
+            </div>
+            <div className='col-md-6 col-12'>
+                <div className='row'>
+                    <div className='col-md-10 offset-md-1'>
+                        <p className="mt-5 text-start  fs-6" style={{ color: "#08A391" }}>AWS  Architecture  Design | ECS  Cloud  Infotech</p>
+                        <h3 className='d-flex text-start display-5 fw-medium' style={{ color: "rgba(0, 0, 0, 0.722)" }}>AWS  Cloud  Solution  Architect</h3>
+                        <p className="pt-3 text-start fs-4">AWS Well-Architected provides a framework to help cloud architects build secure, high-performing, resilient, and efficient architectures.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className='container'>
+        <div className='row pt-5 d-flex'>
+            <div className='col-md-6 col-12'>
+                <div className='row'>
+                    <div className='col-md-10 offset-md-1'>
+                        <p className="mt-5 text-start  fs-6" style={{ color: "#08A391" }}>Efficient  Deployement | Optimal Security | Cost Optimization | High Scalability</p>
+                        <h3 className='d-flex text-start display-5 fw-medium' style={{ color: "rgba(0, 0, 0, 0.722)" }}>Free  Cloud  Migration  Services  on  AWS</h3>
+                        <p className="pt-3 text-start fs-4">Moving your existing data and mission critical workloads to the cloud is essential for the future success of your bussiness.But there is more to it than simply provisioning a server in the cloud</p>
+                    </div>
+                </div>
+            </div>
+            <div className='col-md-6 col-12'>
+                <img className="img-fluid" src={AwsMigration} alt='awsMigration' />
+            </div>
+        </div>
+    </div>
+</section>
+  )
+}
+
+export default CloudServices
---
+++ src/Components/Products.js
@@ -0,0 +1,143 @@
+import React from 'react';
+import EcsCrm from "../assests/ecsCrm.png";
+import EcsBooking from "../assests/booking.png";
+import WhatsappApi from "../assests/api.png";
+import EcsHr from "../assests/ecsHr.png";
+import EcsAttendance from "../assests/attendance.png";
+import FacialRecognition from "../assests/facialRecognition.png";
+import EcsTracking from "../assests/tracking.png";
+import SchoolManagement from "../assests/schoolManagement.png";
+function Products() {
+    return (
+        <section className='container-fluid' style={{ backgroundColor: "#E1FDFA" }}>
+            <div className='container'>
+                <div className='row pt-5 d-flex'>
+                    <div className='col-md-6 col-12'>
+                        <div className='row'>
+                            <div className='col-md-10 offset-md-1'>
+                                <p className="mt-5 text-start  fs-6" style={{ color: "#08A391" }}>ECS CRM Powering Your Success</p>
+                                <h3 className='d-flex text-start display-5 fw-medium' style={{ color: "rgba(0, 0, 0, 0.722)" }}>Revolutionize Your <br></br> Business with ECS CRM</h3>
+                                <p className="pt-3 text-start fs-4">End-to-end,fully customizable CRM solution for <br></br> growing bussiness and enterprises.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-6 col-12' style={{ backgroundColor: "#ffffff", borderRadius: "20px" }}>
+                        <img className="img-fluid" src={EcsCrm} alt='ecsCrm' />
+                    </div>
+                </div>
+            </div>
+            <div className='container'>
+                <div className='row mt-5 d-flex'>
+                    <div className='col-md-6 col-12'>
+                        <img className="img-fluid" src={EcsBooking} alt='ecsBooking' />
+                    </div>
+                    <div className='col-md-6 col-12'>
+                        <div className='row'>
+                            <div className='col-md-10 offset-md-1'>
+                                <p className="mt-5 text-start  fs-6" style={{ color: "#08A391" }}>ECS Appointment Booking Made Easy</p>
+                                <h3 className='d-flex text-start display-5 fw-medium' style={{ color: "rgba(0, 0, 0, 0.722)" }}>Effortless Booking with ECS Appointments</h3>
+                                <p className="pt-3 text-start fs-4">Customers and clients can schedule appointments with the staff members.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='container'>
+                <div className='row mt-5 d-flex'>
+                    <div className='col-md-6 col-12'>
+                        <div className='row'>
+                            <div className='col-md-10 offset-md-1'>
+                                <p className="mt-5 text-start  fs-6" style={{ color: "#08A391" }}>WHATSAPP  CHATBOOT   INTEGRATION   &  API  FOR  ENHANCED   ENGAGEMENT   |   ECS  Cloud  Infotech</p>
+                                <h3 className='d-flex text-start display-5 fw-medium' style={{ color: "rgba(0, 0, 0, 0.722)" }}>Automated Chatbot With Instant Responses</h3>
+                                <p className="pt-3 text-start fs-4">Set up a chatbot as your 24/7 customer service.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-6 col-12'>
+                        <img className="img-fluid" src={WhatsappApi} alt='ecsWhatsappapi' />
+                    </div>
+                </div>
+            </div>
+            <div className='container'>
+                <div className='row mt-5 d-flex'>
+                    <div className='col-md-6 col-12'>
+                        <img className="img-fluid" src={EcsHr} alt='ecsHr' />
+                    </div>
+                    <div className='col-md-6 col-12'>
+                        <div className='row'>
+                            <div className='col-md-10 offset-md-1'>
+                                <p className="mt-5 text-start  fs-6" style={{ color: "#08A391" }}>SMART  HR  |  PAYROLL  |  hiring  |  EMPLOYEE  TRANSPORT  MANAGEMENT  |   PROJECT  MANAGEMENT  SYSTEM</p>
+                                <h3 className='d-flex text-start display-5 fw-medium' style={{ color: "rgba(0, 0, 0, 0.722)" }}>Human  Resource  Management  System</h3>
+                                <p className="pt-3 text-start fs-4">A  Human  Resouce  Managemnet  Systems  (HRMS) is  a  Software  Solutions  that  streamlines  and  automatesvarious  hr  functions,  including  recruitment , employeerecords , payroll , benefits  administration , and  performance managemnet.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='container'>
+                <div className='row pt-5 d-flex'>
+                    <div className='col-md-6 col-12'>
+                        <div className='row pt-5'>
+                            <div className='col-md-10 offset-md-1'>
+                                <h3 className='d-flex text-start display-5 fw-medium' style={{ color: "rgba(0, 0, 0, 0.722)" }}>Mobile  Attendance  Application</h3>
+                                <p className="pt-3 text-start fs-4">Leverage our expertise and get a high-performance and scalable mobile application.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-6 col-12'>
+                        <img className="img-fluid" src={EcsAttendance} alt='ecsAttendance' />
+                    </div>
+                </div>
+            </div>
+            <div className='container'>
+                <div className='row pt-5 d-flex'>
+                <div className='col-md-6 col-12'>
+                        <img className="img-fluid" src={FacialRecognition} alt='ecsFacerecognitionApp' />
+                    </div>
+                    <div className='col-md-6 col-12'>
+                        <div className='row pt-5'>
+                            <div className='col-md-10 offset-md-1'>
+                                <h3 className='d-flex text-start display-5 fw-medium' style={{ color: "rgba(0, 0, 0, 0.722)" }}>Facial  Recognition  Application</h3>
+                                <p className="pt-3 text-start fs-4">Leverage our expertise and get a high-performance and scalable mobile application.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className='container'>
+                <div className='row pt-5 d-flex'>
+                    <div className='col-md-6 col-12'>
+                        <div className='row'>
+                            <div className='col-md-10 offset-md-1'>
+                                <p className="mt-5 text-start  fs-6" style={{ color: "#08A391" }}>APPLICANT  TRACKING  SYSTEM  |   ECS  Cloud  Infotech</p>
+                                <h3 className='d-flex text-start display-5 fw-medium' style={{ color: "rgba(0, 0, 0, 0.722)" }}>An  Applicant   Tracking  System  Built  by Recruiters</h3>
+                                <p className="pt-3 text-start fs-4">Manatal is a comprehensive cloud-based ATS - Applicant Tracking System to make your recruitment process easier. Through effective pipelines, AI-driven insights, and applicant matching, candidate management becomes a seamless task.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='col-md-6 col-12'>
+                        <img className="img-fluid" src={EcsTracking} alt='ecsTracking' />
+                    </div>
+                </div>
+            </div>
+            <div className='container'>
+                <div className='row mt-5 pb-5 d-flex'>
+                    <div className='col-md-6 col-12'>
+                        <img className="img-fluid" src={SchoolManagement} alt='schoolManagement' />
+                    </div>
+                    <div className='col-md-6 col-12'>
+                        <div className='row'>
+                            <div className='col-md-10 offset-md-1'>
+                                <p className="mt-5 text-start  fs-6" style={{ color: "#08A391" }}>SCHOOL  MANAGEMENT   SYSTEM  |   ECS  Cloud  Infotech</p>
+                                <h3 className='d-flex text-start display-5 fw-medium' style={{ color: "rgba(0, 0, 0, 0.722)" }}>School   Management  System  Software</h3>
+                                <p className="pt-3 text-start fs-4">Innovate Teaching, Promote Skill Building, and Achieve Greater Learning Outcomes!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Products
